12fe2e55d40bdf4df043b63d8e35e0ce04cf1910
MarcosRigal <riveragavilanmarcos@gmail.com>
2020-08-04 21:35:40 +0200

    Esta en rojo porque dice que lo he modificado y me pone nervioso

--- 03-operadores/aritmeticos.py
+++ 03-operadores/aritmeticos.py
@@ -1,7 +1,15 @@
 #Operadores aritméticos
-numero1 = 78
-numero2 = 2 #operador asignación =
+numero1 = 77
+numero2 = 44 #operador asignación =
 
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
+resto = numero1 % numero2
+
+print("*********Calculadora**********")
+print(f"La suma es: {numero1 + numero2}")
+print(f"La resta es: {resta}")
+print(f"La multiplicación es: {multiplicacion}")
+print(f"La división es: {division}")
+print(f"El resto es: {resto}")
---
+++ 03-operadores/asignación.py
@@ -0,0 +1,32 @@
+# Operadores de asignación
+
+edad = 55
+
+print(edad)
+
+edad += 5
+edad = edad + 5
+edad -=5
+
+print(edad)
+
+#Operadores incremento y decremento
+
+year = 2021
+
+# Incremento
+
+year +=1
+
+# Decremento 
+
+year -= 1
+
+# Pre incremento
+
+year = 1 + year
+
+# Pre decremento
+
+year = 1 - year
+
